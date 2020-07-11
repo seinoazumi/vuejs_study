6d2adb932b95334d757636aa41f13d9b443ae6b5
Azumi S <sazumy@users.noreply.github.com>
2020-07-11 17:16:45 +0900

    chap6

---
+++ chap6/hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p class="greeting">メッセージ：{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: "こんにちは"
+      }
+    }
+  }
+</script>
+
+<style>
+  .greeting {
+    color: #42b983;
+  }
+</style>
---
+++ chap6/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="message">
+    メッセージ
+  </p>
+</template>
+
+<style scoped>
+  .message { color: #42b983; }
+</style>
