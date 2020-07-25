e319a77692ebe5094e0067c5e6b4182903fea538
Azumi S <sazumy@users.noreply.github.com>
2020-07-25 14:04:43 +0900

    buils sample app 4-1

---
+++ design_chap4/01_omikuji.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>おみくじ</h1>
+    <div v-if="result">結果は…「{{ result }}」です！</div>
+    <button class="button" v-on:click="draw">{{ button }}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        result: null,
+        button: 'おみくじを引く'
+      }
+    },
+    methods: {
+      draw: function(){
+        var omikuji = ['大吉', '吉', '凶'];
+        var index = Math.floor(Math.random() * omikuji.length);
+        
+        this.result = omikuji[index];
+        this.button = 'おみくじをもう一度引く'
+      }
+    }
+  }
+</script>
