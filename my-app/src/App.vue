380d4bb47b6867662882c0e7f591dce28b4b62dd
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-19 22:26:09 +0900

    vue-routerのインストール

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,31 +1,12 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
-    <EditForm />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import EditForm from '@/components/EditForm.vue'
-
-export default {
-  name: 'app',
-  components: {
-    EditForm
-  },
-  computed: {
-    // ローカルのmessageとストアのmessageを同期
-    message() { return this.$store.getters.message }
-  },
-  // created() {
-  //   // ストアの状態を取得
-  //   console.log(this.$store.state.count)
-  //   // ストアの状態を更新
-  //   this.$store.commit('increment')
-  // }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -34,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
